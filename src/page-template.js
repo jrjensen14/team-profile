b6ea168cce8025ec7410a2d3d810c0a7d5bc2f02
JRJ <jjsocc171@msn.com>
2021-01-06 23:31:13 -0600

    user questions

--- src/page-template.js
+++ src/page-template.js
@@ -1,29 +1,39 @@
-const fs = require('fs');
+
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
+
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
 
-const generatePage = (name, github) => {
-    return `
-    <!DOCTYPE html> 
-    <html lang="en"> 
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <title>Portfolio Demo</title>
-    </head>
+// const generateProfile = team => {
+//     return `
+//     <!DOCTYPE html> 
+//     <html lang="en"> 
+//     <head>
+//       <meta charset="UTF-8">
+//       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//       <meta http-equiv="X-UA-Compatible" content="ie=edge">
+//       <title>Portfolio Demo</title>
+//     </head>
   
-    <body>
-      <h1>${name}</h1>
-      <h2><a href="https://github.com/${github}">Github</a></h2>
-    </body>
-    </html>
-    `;
-  };
+//     <body>
+//       <h1>${name}</h1>
+//       <h2><a href="https://github.com/${github}">Github</a></h2>
+//     </body>
+//     </html>
+//     `;
+//   };
   
-  fs.writeFile('./index.html', generatePage(), err => {
-    if (err) throw new Error(err);
+// //   fs.writeFile('./dist/team.html', generateProfile(), err => {
+// //     if (err) throw new Error(err);
   
-    console.log('Portfolio complete! Check out index.html to see the output!');
-  });
+// //     console.log('Portfolio complete! Check out index.html to see the output!');
+// //   });
 
-  module.exports = generatePage;
+//   module.exports = generateProfile;
