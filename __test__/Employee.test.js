2c81127079bc334b4f3d7cefd3e2b2ac41be15a5
JRJ <jjsocc171@msn.com>
2021-01-09 13:55:06 -0600

    added employee properties/methods

--- __test__/Employee.test.js
+++ __test__/Employee.test.js
@@ -1,1 +1,12 @@
-const Employee = require('../lib/Employee')
+const Employee = require('../lib/Employee');
+
+// Employee Properies:
+// Name, Id, Email, 
+
+test('create Employee name', () => {
+    const name = new Name();
+
+    expect(name.)
+})
+// Employee Methods:
+// getName(), getId(), getEmail(), getRole()-returns 'Employee'
