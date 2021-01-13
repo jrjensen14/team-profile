718f60c8ce8a65b463b97fff6c6f4987b44f2c99
JRJ <jjsocc171@msn.com>
2021-01-12 19:04:50 -0600

    feature/manager

--- __test__/Manager.test.js
+++ __test__/Manager.test.js
@@ -1,1 +1,22 @@
 const Manager = require('../lib/Manager');
+
+// Manager Prperties: Employees properties & officeNumber
+// Manager Methods: gitRole(), -Overridden to return 'Manager'
+
+test ('create office numeber property', () => {
+    const officeNumber = 100;
+    const mang = new Manager("Kat", 1, "test@test.com", officeNumber);
+    expect(mang.officeNumber).toBe(officeNumber);
+});
+
+test ('set office number function', () => {
+    const officeNumber = 100;
+    const mang = new Manager("Kat", 1, "test@test.com", 100);
+    expect(mang.getOfficeNumber()).toBe(officeNumber);
+});
+
+test ('set manager getRole() to Manager', () => {
+    const role = "Manager";
+    const mang = new Manager("Kat", 1, "test@test.com");
+    expect(mang.getRole()).toBe(role);
+});
